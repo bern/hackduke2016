7db966e8858afccb87aa2cb0509cc2cd6e6b0ca7
bern422 <bern422@gmail.com>
2016-11-20 09:58:43 -0500

    added sounds

--- js/buffaloJumpApp.js
+++ js/buffaloJumpApp.js
@@ -20,24 +20,27 @@ angular.module('buffaloJumpApp',[])
 				,descriptors: [
 					{
 						position: "3.2 2 1" // "x y z"
-						,soundSrc: "sounds/test.wav"
+						,soundSrc: "sounds/man.wav"
 						,mouseEnterScale: "2 2 1"
 						,mouseLeaveScale: "1 1 1"
 						,id: "man"
+						,src: "images/sound.png"
 					},
 					{
 						position: "3.2 2 -2" // "x y z"
-						,soundSrc: "sounds/test.wav"
+						,soundSrc: "sounds/car.wav"
 						,mouseEnterScale: "2 2 1"
 						,mouseLeaveScale: "1 1 1"
 						,id: "car"
+						,src: "images/car_asset.png"
 					},
 					{
 						position: "-0 3.5 11" // "x y z"
-						,soundSrc: "sounds/test.wav"
+						,soundSrc: "sounds/sunny.wav"
 						,mouseEnterScale: "2 2 1"
 						,mouseLeaveScale: "1 1 1"
-						,id: "sun"
+						,id: "sunny"
+						,src: "images/sound.png"
 					}
 				]
 			}
@@ -63,27 +66,31 @@ angular.module('buffaloJumpApp',[])
 						,mouseEnterScale: "2 2 1"
 						,mouseLeaveScale: "1 1 1"
 						,id: "jump"
+						,src: "images/sound.png"
 					},
 					{
 						position: "-0 3.5 11" // "x y z"
-						,soundSrc: "sounds/test.wav"
+						,soundSrc: "sounds/cloudy.wav"
 						,mouseEnterScale: "2 2 1"
 						,mouseLeaveScale: "1 1 1"
-						,id: "clouds"
+						,id: "cloudy"
+						,src: "images/sound.png"
 					},
 					{
 						position: "1.6 0 7" // "x y z"
-						,soundSrc: "sounds/test.wav"
+						,soundSrc: "sounds/grass.wav"
 						,mouseEnterScale: "2 2 1"
 						,mouseLeaveScale: "1 1 1"
 						,id: "grass"
+						,src: "images/sound.png"
 					},
 					{
 						position: "-2.7 1.7 2" // "x y z"
-						,soundSrc: "sounds/test.wav"
+						,soundSrc: "sounds/mountains.wav"
 						,mouseEnterScale: "2 2 1"
 						,mouseLeaveScale: "1 1 1"
 						,id: "mountains"
+						,src: "images/sound.png"
 					}
 				]
 			}
@@ -153,15 +160,24 @@ angular.module('buffaloJumpApp',[])
 		$scope.descMouseEnter = function() {
 			console.log(this);
 		}
+
+		$scope.globalStop = function(){
+			console.log("here")
+    	if ($scope.audio !== undefined && $scope.audio !== null) {
+				$scope.audio.pause();
+				$scope.audio = null;
+			}
+		}
+
 		$scope.descClick = function(descriptor){
 			// Play audio here.
-			console.log(descriptor.audio)
-			if (descriptor.audio === undefined || descriptor.audio === null) {
-				descriptor.audio = new Audio(descriptor.soundSrc);
-				descriptor.audio.play();
+			console.log(descriptor)
+			if ($scope.audio === undefined || $scope.audio === null) {
+				$scope.audio = new Audio(descriptor.soundSrc);
+				$scope.audio.play();
 			} else {
-				descriptor.audio.pause();
-				descriptor.audio = null;
+				$scope.audio.pause();
+				$scope.audio = null;
 			}
 		}
 
